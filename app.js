647efbd04e148a3559d53b04c494c82c75ac5f3e
Nitin kumar <nitinkumar.jgd.080@gmail.com>
2026-04-24 00:58:02 +0530

    demo change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Secure CI/CD 🚀');
+  res.send('Hello Secure CI/CD Demo 🚀');
 });
 
 app.listen(3000, () => {
